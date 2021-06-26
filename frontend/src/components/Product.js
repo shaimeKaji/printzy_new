e4173e8101b8435cfa5c2cf4bf7d4b31884b0156
Shaime Siraj <kajishaime@gmail.com>
2021-06-26 23:51:49 +0530

    r

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,7 +7,8 @@ export default function Product(props) {
   return (
     <div key={product._id} className="card">
       <Link to={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt={product.name} />
+        <img className="medium" src={product.image} alt={product.name} 
+                />
       </Link>
       <div className="card-body">
         <Link to={`/product/${product._id}`}>
