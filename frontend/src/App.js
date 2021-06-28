c034ef48226a16d017f6e34e92a35cf1b0a7ae48
Shaime Siraj <kajishaime@gmail.com>
2021-06-28 22:47:18 +0530

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -122,9 +122,6 @@ function App() {
                 </Link>
                 <ul className="dropdown-content">
                   <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                  </li>
-                  <li>
                     <Link to="/productlist">Products</Link>
                   </li>
                   <li>
