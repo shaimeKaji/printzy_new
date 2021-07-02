66f7ce76358da22f1eee078677870d006795ad33
Shaime Siraj <kajishaime@gmail.com>
2021-07-02 20:03:45 +0530

    WD

--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -38,17 +38,7 @@ export default function PaymentMethodScreen(props) {
           </div>
         </div>
         <div>
-          <div>
-            <input
-              type="radio"
-              id="stripe"
-              value="Stripe"
-              name="paymentMethod"
-              required
-              onChange={(e) => setPaymentMethod(e.target.value)}
-            ></input>
-            <label htmlFor="stripe">Stripe</label>
-          </div>
+          
         </div>
         <div>
           <label />
